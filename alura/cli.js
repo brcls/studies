d6ec3a952cf1f001d6ce3f6896c29d9d8b9e0a0d
brcls <petrick_barcelos@hotmail.com>
2022-08-08 17:35:14 -0300

    feat: finish link object with status code

--- alura/cli.js
+++ alura/cli.js
@@ -1,11 +1,17 @@
 const chalk = require("chalk");
-const pegarArquivo = require("./index");
+const pegaArquivo = require("./index");
+const validaURLs = require("./http-validacao");
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoDeArquivo) {
-  const resultado = await pegarArquivo(caminhoDeArquivo[2]);
-  console.log(chalk.yellow("Lista de links"), resultado);
+  const resultado = await pegaArquivo(caminhoDeArquivo[2]);
+
+  if (caminho[3] === "validar") {
+    console.log(chalk.blue("Links validados"), await validaURLs(resultado));
+  } else {
+    console.log(chalk.yellow("Lista de links"), resultado);
+  }
 }
 
 processaTexto(caminho);
