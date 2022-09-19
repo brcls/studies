0f8f2882212518aec3d8d377c20e5685fb37f74f
brcls <petrick_barcelos@hotmail.com>
2022-09-19 15:24:04 -0300

    feat: finish polymorphism and start duck typing

--- alura/4 - Python: avançando na orientação a objetos/modelo.py
+++ alura/4 - Python: avançando na orientação a objetos/modelo.py
@@ -1,57 +1,88 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano}: {self._likes} Likes'
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    def dar_likes(self):
-        self.likes += 1
 
     def __str__(self):
-        return f'Nome {self.__nome}, ano {self.ano}, duração {self.duracao} - Likes {self.__likes}'
+        return f'{self._nome} - {self.ano} - {self.duracao} min: {self._likes} Likes'
 
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    def dar_likes(self):
-        self.likes += 1
 
     def __str__(self):
-        return f'Nome: {self.nome} ({self.ano}) - {self.temporadas} temporadas - Likes: {self.likes}'
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas: {self._likes} Likes'
+
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(vingadores)
-
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 atlanta = Serie('atlanta', 2018, 2)
-print(atlanta)
+
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+
+demolidor.dar_like()
+demolidor.dar_like()
+
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta, tmep, demolidor]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
+    print(programa)
